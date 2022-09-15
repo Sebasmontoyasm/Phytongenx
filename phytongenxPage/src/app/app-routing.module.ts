91f392750c312ad52a1bdb8c4db1e07bea408e90
sebasmontoyasm <sebasmontoyasm@gmail.com>
2022-09-15 00:17:14 -0500

    Paths new components

--- phytongenxPage/src/app/app-routing.module.ts
+++ phytongenxPage/src/app/app-routing.module.ts
@@ -4,13 +4,24 @@ import { MasterDataPageComponent } from './components/master-data-page/master-da
 import { SinginPageComponent } from './components/singin-page/singin-page.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { CmsPageComponent } from './components/cms-page/cms-page.component';
+import { CmsPerformancePageComponent } from './components/cms-performance-page/cms-performance-page.component';
+import { QbPageComponent } from './components/qb-page/qb-page.component';
+import { QbPerformancePageComponent } from './components/qb-performance-page/qb-performance-page.component';
+import { CmsDetailPageComponent } from './components/cms-detail-page/cms-detail-page.component';
+import { QbDetailPageComponent } from './components/qb-detail-page/qb-detail-page.component';
 
 const routes: Routes = [
   { path:'', redirectTo:'/HomePage', pathMatch:'full'},
   { path:'HomePage', component: HomePageComponent},
   { path:'get', component: MasterDataPageComponent},
   { path:'singin', component: SinginPageComponent},
-  { path:'cms', component: CmsPageComponent}
+  { path:'cms', component: CmsPageComponent},
+  { path:'cms/performance', component: CmsPerformancePageComponent},
+  { path:'cms/detail/:id?', component: CmsDetailPageComponent},
+  { path:'qb', component: QbPageComponent},
+  { path:'qb/performance', component: QbPerformancePageComponent},
+  { path:'qb/detail/:id?', component: QbDetailPageComponent},
+
 ];
 
 @NgModule({
