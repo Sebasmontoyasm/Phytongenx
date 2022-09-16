e78f33d60fc52105eef70c310c67557d6deb50ab
sebasmontoyasm <sebasmontoyasm@gmail.com>
2022-09-16 15:17:01 -0500

    Data Interface for MasterData Page

--- phytongenxPage/src/app/interfaces/data.ts
+++ phytongenxPage/src/app/interfaces/data.ts
@@ -1,2 +1,26 @@
-export interface Data {
-}
+/**
+ * MasterData for MasterData View Page
+ * ID number,
+ * PO_Number string,
+ * Date_CSM_Processed string,
+ * NamePDF string,
+ * Invoice_Number number,
+ * Date_Quickbooks_Processed string,
+ * Date_invoice_recieved string,
+ * NamePDF string
+ **/
+
+ export interface Data{
+    id:number;
+    po_number?: string;
+    dateCSM?: string;
+    namePO?: string;
+    invoice?: string;
+    dateQP?: string;
+    dateInv?: string;
+    PDA_pdf?: string;
+    invoceObj?: number;
+    po_numberObj?:string;
+    idObj?: number;
+    comments?: string;
+  }
