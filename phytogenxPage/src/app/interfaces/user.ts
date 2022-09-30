f065a8d2a0324078fdc9f0d6cf042c34310774f1
sebasmontoyasm <sebasmontoyasm@gmail.com>
2022-09-29 21:26:13 -0500

    Interface users with type roles for access.

--- phytogenxPage/src/app/interfaces/user.ts
+++ phytogenxPage/src/app/interfaces/user.ts
@@ -1,12 +1,13 @@
+export type Roles = 'administrator'|'cms'|'qb'|'guest';
+
 export interface User {
     username: string;
     password: string;
-    lastlogin: Date;
 }
 
 export interface UserResponse {
     message: string;
     token: string;
     id: number;
-    rol: string;
+    rol: Roles;
 }
