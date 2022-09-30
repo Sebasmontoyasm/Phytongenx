697d64900af599ed16acfb0733dac6fcbb1869bf
sebasmontoyasm <sebasmontoyasm@gmail.com>
2022-09-29 21:22:14 -0500

    unmerge app app.component in header component

---
+++ phytogenxPage/src/app/components/header/header.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog} from '@angular/material/dialog';
+import { AuthService } from 'src/app/services/auth.service';
+import { SinginPageComponent } from '../singin-page/singin-page.component';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  isAdmin = false;
+  isLogged = false;
+  constructor(public singin: MatDialog,private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.authService.isLogged.subscribe((res) => (this.isLogged = res));
+  }
+
+  onSinginout(): void{
+    this.authService.loginout();
+
+  }
+
+  closeSingIn(){
+    this.singin.closeAll();
+  }
+  openSingIn(enterAnimationDuration: string, exitAnimationDuration: string): void {
+    this.singin.open(SinginPageComponent, {
+    position: {top: '130px'},
+    width: '40%',
+    height: '69%',
+    
+    enterAnimationDuration,
+    exitAnimationDuration
+  });
+  }
+}
