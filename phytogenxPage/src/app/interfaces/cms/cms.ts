8933d72056b87395f635513d8acb2d82ff4c6d34
sebasmontoyasm <sebasmontoyasm@gmail.com>
2022-10-14 21:18:16 -0500

    Inteface for post and patch send form.

--- phytogenxPage/src/app/interfaces/cms/cms.ts
+++ phytogenxPage/src/app/interfaces/cms/cms.ts
@@ -2,3 +2,20 @@ export interface Cms{
     id:number;
     po_number: string;
 }
+
+export interface CmsUpdate{
+    reason: string;
+    comment: string;
+    Date_CSM_Processed: Date;
+    PDF_Name: string;
+}
+
+export interface CmsCreate{
+    ponumber:string;
+    Date_CSM_Processed: Date;
+    PDF_Name: string;
+}
+
+
+
+
--- phytogenxPage/src/app/interfaces/qb/qb.ts
+++ phytogenxPage/src/app/interfaces/qb/qb.ts
@@ -1,2 +1,12 @@
 export interface Qb {
+    id:number;
+    PO_Number: string;
+    Invoice: string;
+}
+
+export interface QbUpdate{
+    comment:string;
+    Invoice: string;
+    NamePDF: string;
+    Date_invoice: string;
 }
