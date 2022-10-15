0407eada0bfc33d9f8dac964e1847dda78dc646c
sebasmontoyasm <sebasmontoyasm@gmail.com>
2022-10-14 21:20:39 -0500

    get, and upload services with handlerError

--- phytogenxPage/src/app/services/qb/qb.service.ts
+++ phytogenxPage/src/app/services/qb/qb.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { QbUpdate } from 'src/app/interfaces/qb/qb';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +20,25 @@ export class QbService {
   
   detail(id: string){
     return this.http.get(this.url+"detail/"+id); 
+  }
+
+  get(){
+    return this.http.get(`${environment.API_URL}/masterdata/qb`);   
+  }
+
+  update(id:number, qbUpdate:QbUpdate): Observable<any>{
+    return this.http.patch<any>(`${environment.API_URL}/masterdata/qb/${id}`,qbUpdate)
+    .pipe(
+      catchError(this.handlerError)
+    );
+  }
+
+  private handlerError(err:any): Observable<never> {
+    let errorMessage = 'An error occurred retrienving data';
+    if(err){
+      errorMessage= `Error: code ${err.message}`
+    }
+    window.alert(errorMessage)  
+    return throwError(errorMessage);
   }
 }
