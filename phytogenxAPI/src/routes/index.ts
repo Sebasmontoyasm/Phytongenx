b314ee29a396d905ca8e6255b344cacefa35bbd2
sebasmontoyasm <sebasmontoyasm@gmail.com>
2022-10-14 21:06:21 -0500

    add masterdata router controller

--- phytogenxAPI/src/routes/index.ts
+++ phytogenxAPI/src/routes/index.ts
@@ -1,10 +1,13 @@
 import {Router} from 'express';
 import auth from './auth';
 import user from './user';
+import masterdata from './masterdata';
 
 const routes = Router();
 
-routes.use('/auth',auth);
+routes.use('/masterdata', masterdata);
+routes.use('/auth', auth);
 routes.use('/users', user);
+
 
 export default routes;
