e2eecb3d074846a4bc1d5300c9f34d4119b7f68a
sebasmontoyasm <sebasmontoyasm@gmail.com>
2022-10-31 05:03:08 -0500

    Check PDF in Qb and CMS Route and upload file with middlewares

--- phytogenxAPI/src/routes/cms.ts
+++ phytogenxAPI/src/routes/cms.ts
@@ -2,12 +2,15 @@ import { CMSController  } from "../controller/CMSController";
 import { checkJwt } from '../middlewares/jwt';
 import { checkRole } from '../middlewares/rol';
 import { Router } from 'express';
+import { uploadFile } from "../middlewares/uploadfile";
 
 const router = Router();
 
 router.get('',[checkJwt,checkRole(['cms','administrator'])], CMSController.getAll);
 router.get('/performance',[checkJwt,checkRole(['cms','administrator'])], CMSController.performance);
 router.get('/:ponumber',[checkJwt,checkRole(['cms','administrator'])], CMSController.getById);
+router.get('/pdf/:pdfname',[checkJwt,checkRole(['cms','administrator'])], CMSController.getPDF_Name);
 router.patch('/:id',[checkJwt,checkRole(['cms','administrator'])], CMSController.update);
+router.post('/upload',[checkJwt,checkRole(['cms','administrator']),uploadFile('cms')], CMSController.upload);
 
 export default router;
--- phytogenxAPI/src/routes/qb.ts
+++ phytogenxAPI/src/routes/qb.ts
@@ -2,12 +2,15 @@ import { checkJwt } from '../middlewares/jwt';
 import { checkRole } from '../middlewares/rol';
 import { Router } from 'express';
 import { QbController } from "../controller/QbController";
+import { uploadFile } from '../middlewares/uploadfile';
 
 const router = Router();
 
 router.get('',[checkJwt,checkRole(['qb','administrator'])],QbController.getAll);
 router.get('/performance',[checkJwt,checkRole(['qb','administrator'])],QbController.getPerformance);
 router.get('/:invoice',[checkJwt,checkRole(['qb','administrator'])],QbController.getByInvoice);
+router.get('/pdf/:namepdf',[checkJwt,checkRole(['qb','administrator'])],QbController.getNamePDF);
 router.patch('/:id',[checkJwt,checkRole(['qb','administrator'])],QbController.update);
+router.post('/upload',[checkJwt,checkRole(['qb','administrator']),uploadFile('qb')], QbController.upload);
 
 export default router;
